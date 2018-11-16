3d11d2e2e826f9bef8b91df20b80c7df1bf0c0cc
Hemanth <hemaanth01@gmail.com>
2018-11-15 23:05:33 -0600

    Added results to screen

--- BaseTest/src/main/java/common/BaseTest.java
+++ BaseTest/src/main/java/common/BaseTest.java
@@ -1,16 +1,23 @@
 package common;
 
+import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
@@ -29,6 +36,9 @@ public class BaseTest {
 	public static ExtentReports extent;
 	public static ExtentTest logger;
 	public final String TESTDATAPATH = ".\\src\\test\\java\\testData\\";
+	public final String REPORT_NAME = "Apsrtc_Results";
+	public final String REPORT_TITLT = "Apsrtc Regression Testing";
+	public final String REPORT_HEADING = "[APSRTC]";
 
 	@Parameters({ "browser", "ipAddress" })
 	@BeforeClass
@@ -73,20 +83,34 @@ public class BaseTest {
 	public WebDriver getDriver() {
 		return driver;
 	}
+
 	public ExtentTest getLogger() {
 		return logger;
 	}
+
 	public ExtentReports getExtent() {
 		return extent;
 	}
+
 	@BeforeSuite
-	public void reportInitiliaze() {
-		htmlreport = new ExtentHtmlReporter("./reports/GROUPON_Results.html");
-		htmlreport.config().setDocumentTitle("Groupon Regression Testing");
-		htmlreport.config().setReportName("[GROUPON]");
+	public void reportInitiliaze() throws IOException {
+		cleanUpDirectory("./reports");
+		htmlreport = new ExtentHtmlReporter("./reports/" + REPORT_NAME + ".html");
+		htmlreport.config().setDocumentTitle(REPORT_TITLT);
+		htmlreport.config().setReportName(REPORT_HEADING);
 		extent = new ExtentReports();
 		extent.attachReporter(htmlreport);
 
+	}
+
+	@AfterSuite
+	public void afterSuite1() throws IOException {
+		File htmlFile = new File(System.getProperty("user.dir")+"/reports/" + REPORT_NAME + ".html");
+		Desktop.getDesktop().browse(htmlFile.toURI());
+	}
+
+	public void cleanUpDirectory(String directory) throws IOException {
+		Files.walk(Paths.get(directory)).filter(Files::isRegularFile).map(Path::toFile).forEach(File::delete);
 	}
 
 	/*
